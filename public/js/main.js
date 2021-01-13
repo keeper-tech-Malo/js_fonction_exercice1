630c9ee7e130dcc12a06d9546f7040a1eb0b692b
Malo <ndekezi37@gmail.com>
2021-01-13 14:06:08 +0100

    moitie du travail

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+// exo1
+// let idendite = (a,b) => {
+//     return  a + b
+// }
+
+// let nb = idendite(10,20);
+// console.log(nb);
+
+// // exo2
+// let identite = (a,b) => {
+//     return b - a
+// }
+// let nb = identite(10,20)
+// console.log(nb);
+
+// exo3
+// let identite = (a,b) => {
+//     return b * a
+// }
+// let nb = identite(10,20)
+// console.log(nb);
+
+// // exo4
+// let identite = (a,b) => {
+//     return b / a
+// }
+// let nb = identite(10,20)
+// console.log(nb);
+
+
+
